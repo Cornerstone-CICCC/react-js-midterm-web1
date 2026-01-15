dbf6fe45cba328940873b4eb6a34e521c1b93388
Miya Kuo <miya3927@gmail.com>
2026-01-14 19:23:25 -0800

    Finished basic layout for products listing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PageLayout from "./layouts/PageLayout";
+import Products from "./pages/Products/Products";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<PageLayout />}>
           <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
         </Route>
       </Routes>
     </BrowserRouter>
